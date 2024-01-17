34da5cffac1a95cf540645138263ccaefe4c963b
Thiago Leite <leite.tjs@gmail.com>
2024-01-17 18:36:46 -0300

    feat: login form

---
+++ actions/login.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import { LoginSchema } from '@/schemas';
+import { z } from 'zod';
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  console.log(values);
+
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: 'Invalid Fields!' };
+  }
+
+  return { success: 'Email sent' };
+};
