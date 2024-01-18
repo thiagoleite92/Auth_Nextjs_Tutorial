da4144b61d62693d05e98eebac52f9d135970968
Thiago Leite <leite.tjs@gmail.com>
2024-01-18 20:56:01 -0300

    feat: middleware for auth routes, public routes and authentication routes

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const font = Poppins({
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6">
+      <div className="flex  flex-col items-center justify-center space-y-6 ">
         <h1
           className={cn(
             'text-6xl font-semibold text-white drop-shadow-md',
